8edfc6d8e18198763852ead8eea1d36f168c95f3
Bradley Guidry <bradleyguidry@gmail.com>
2021-01-02 22:16:18 -0600

    npm install and api routes

--- routes/api.js
+++ routes/api.js
@@ -15,8 +15,7 @@ router.put("/api/workouts/:id", ({ body, params }, res) => {
   Workout.findByIdAndUpdate(
     params.id,
     { $push: { exercises: body } },
-    // "runValidators" will ensure new exercises meet our schema requirements
-    { new: true, runValidators: true }
+   { new: true, runValidators: true }
   )
     .then(dbWorkout => {
       res.json(dbWorkout);
@@ -25,8 +24,7 @@ router.put("/api/workouts/:id", ({ body, params }, res) => {
       res.json(err);
     });
 });
-
-router.get("/api/workouts", (req, res) => {
+  router.get("/api/workouts", (req, res) => {
   Workout.find()
     .then(dbWorkouts => {
       res.json(dbWorkouts);
@@ -47,7 +45,7 @@ router.get("/api/workouts/range", (req, res) => {
     });
 });
 
-router.delete("/api/workouts", ({ body }, res) => {
+  router.delete("/api/workouts", ({ body }, res) => {
   Workout.findByIdAndDelete(body.id)
     .then(() => {
       res.json(true);
