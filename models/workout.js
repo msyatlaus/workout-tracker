edd2a4a32690dc490ab67dc926008a6724cc24c6
Bradley Guidry <bradleyguidry@gmail.com>
2021-01-03 16:06:00 -0600

    delete duplicate public folder

--- models/workout.js
+++ models/workout.js
@@ -6,50 +6,47 @@ const workoutSchema = new Schema(
   {
     day: {
       type: Date,
-      default: () => new Date()
+      default: () => new Date(),
     },
     exercises: [
       {
         type: {
           type: String,
           trim: true,
-          required: "Enter an exercise type"
+          required: "Enter an exercise type",
         },
         name: {
           type: String,
           trim: true,
-          required: "Enter an exercise name"
+          required: "Enter an exercise name",
         },
         duration: {
           type: Number,
-          required: "Enter an exercise duration in minutes"
+          required: "Enter an exercise duration in minutes",
         },
         weight: {
-          type: Number
+          type: Number,
         },
         reps: {
-          type: Number
+          type: Number,
         },
         sets: {
-          type: Number
+          type: Number,
         },
         distance: {
-          type: Number
-        }
-      }
-    ]
+          type: Number,
+        },
+      },
+    ],
   },
   {
     toJSON: {
-      // include any virtual properties when data is requested
-      virtuals: true
-    }
+      virtuals: true,
+    },
   }
 );
 
-// adds a dynamically-created property to schema
 workoutSchema.virtual("totalDuration").get(function () {
-  // "reduce" array of exercises down to just the sum of their durations
   return this.exercises.reduce((total, exercise) => {
     return total + exercise.duration;
   }, 0);
